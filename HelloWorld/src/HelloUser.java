f20ffbc3aff0eb41f8aaf9dc9babfec8458de06f
Dornberger <thomas.dornberger@atos.net>
2016-11-16 19:29:27 +0100

    this is my commit msg, made HelloWorld great again..

---
+++ HelloWorld/src/HelloUser.java
@@ -0,0 +1,14 @@
+
+public class HelloUser {
+	private String name;
+	public HelloUser(String name)
+	{
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void greetUser(){
+		System.out.println("Hello " + name + "!");
+	}
+}
