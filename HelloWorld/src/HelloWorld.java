b669cb1ecee571a9ed6e04868f1791c5cdfb0b6a
Dornberger <thomas.dornberger@atos.net>
2016-11-16 19:17:37 +0100

    ok here my commit message made helloWorld great again...

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,18 +1,19 @@
-/**
- * 
- */
+import java.io.IOException;
+import java.util.Scanner;
 
 /**
- * @author Mohsen
+ * @author Thomas
  *
  */
 public class HelloWorld {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("Hello ASE2016 - how are you? :)");
+		System.out.print("Please enter your name: ");
+		HelloUser myself = new HelloUser(scanner.next());
+		myself.greetUser();
+		scanner.close();
 	}
 
 }
